4307bb1032bd61f0ec6f4fb47b81389c1537deb4
Antoine Crone <127962080+Anto1n3c@users.noreply.github.com>
2023-12-12 15:14:24 +0100

    Create declaration.h

---
+++ declaration.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_PLAYERS 4
+
+typedef struct {
+  char nom[50];
+
+} 
+Joueur;
+
+// Function prototype
+void creer_jeu_de_cartes(int n);
